2e99f01a64858117468babe0de4883df48836c01
Jonah Fang <499233138@qq.com>
2025-09-13 09:07:33 +0800

    Update utils.lua

--- lua/tide/utils.lua
+++ lua/tide/utils.lua
@@ -58,12 +58,26 @@ M.generate_unique_names = function(file_paths)
     end
   end
 
-  -- Step 2
+  -- Step 2: Ensure uniqueness by adding parent directories to both files if necessary
   for filename, data in pairs(name_map) do
+    -- if data.count == 1 then
+    --   -- If filename is unique, simply add it to the result
+    --   result[data.paths[1]] = filename
+    -- else
+      -- If filename is not unique, add parent directories to all the conflicting paths
+      local unique_names = {}
       for _, path in ipairs(data.paths) do
+        local unique_name = filename
         local parent = vim.fn.fnamemodify(path, ":h:t") -- Get the parent directory (tail part)
-        result[path] = parent .. "/" .. filename
-      end
+        --while unique_names[unique_name] or result[unique_name] do
+          -- If name is still not unique, add more parent directories
+          unique_name = parent .. "/" .. unique_name
+          --parent = vim.fn.fnamemodify(vim.fn.fnamemodify(path, ":h"), ":h:t") -- Go up one more level
+        --end
+        unique_names[unique_name] = true -- Mark this name as used
+        result[path] = unique_name -- Assign the unique name to the path
+      --end
+    end
   end
 
   return result
